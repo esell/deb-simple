47b22623dbd7846faaae186b91f990d6d381dc69
esell <esell@esheavyindustries.com>
2016-02-07 13:31:04 -0700

    move a few things around

--- main.go
+++ main.go
@@ -14,11 +14,13 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	_ "net/http/pprof"
 	"os"
 	"strconv"
 	"strings"
 
 	"github.com/blakesmith/ar"
+	"github.com/pkg/profile"
 )
 
 type semaphore chan int
@@ -42,6 +44,7 @@ var configFile = flag.String("c", "conf.json", "config file location")
 var parsedConfig = &Conf{}
 
 func main() {
+	defer profile.Start(profile.MemProfile, profile.ProfilePath("."), profile.NoShutdownHook).Stop()
 	flag.Parse()
 	file, err := ioutil.ReadFile(*configFile)
 	if err != nil {
@@ -165,7 +168,14 @@ func inspectPackageControl(filename bytes.Buffer) string {
 }
 
 func createPackagesGz(config *Conf, arch string) bool {
-	var packBuf bytes.Buffer
+	outfile, err := os.Create(config.RootRepoPath + "/dists/stable/main/binary-" + arch + "/Packages.gz")
+	if err != nil {
+		log.Println("error creating packages.gz file")
+		return false
+	}
+	defer outfile.Close()
+	gzOut := gzip.NewWriter(outfile)
+	defer gzOut.Close()
 	// loop through each directory
 	// run inspectPackage
 	dirList, err := ioutil.ReadDir(config.RootRepoPath + "/dists/stable/main/binary-" + arch)
@@ -175,6 +185,7 @@ func createPackagesGz(config *Conf, arch string) bool {
 	}
 	for _, debFile := range dirList {
 		if strings.HasSuffix(debFile.Name(), "deb") {
+			var packBuf bytes.Buffer
 			tempCtlData := inspectPackage(config.RootRepoPath + "/dists/stable/main/binary-" + arch + "/" + debFile.Name())
 			packBuf.WriteString(tempCtlData)
 			packBuf.WriteString("Filename: " + "dists/stable/main/binary-" + arch + "/" + debFile.Name() + "\n")
@@ -193,19 +204,11 @@ func createPackagesGz(config *Conf, arch string) bool {
 			sha256sum := hex.EncodeToString(sha256hash.Sum(nil))
 			packBuf.WriteString("SHA256: " + sha256sum + "\n")
 			packBuf.WriteString("\n\n")
+			gzOut.Write(packBuf.Bytes())
 		}
 	}
 
-	outfile, err := os.Create(config.RootRepoPath + "/dists/stable/main/binary-" + arch + "/Packages.gz")
-	if err != nil {
-		log.Println("error creating packages.gz file")
-		return false
-	}
-	gzOut := gzip.NewWriter(outfile)
-	gzOut.Write(packBuf.Bytes())
 	gzOut.Flush()
-	defer outfile.Close()
-	defer gzOut.Close()
 	return true
 }
 
@@ -220,18 +223,21 @@ func uploadHandler(config Conf) http.Handler {
 			file, header, err := r.FormFile("file")
 			if err != nil {
 				log.Println("error parsing file: ", err)
+				return
 			}
+			defer file.Close()
 			log.Println("filename is: ", header.Filename)
 			out, err := os.Create(config.RootRepoPath + "/dists/stable/main/binary-" + archType + "/" + header.Filename)
 			if err != nil {
 				log.Println("error creating file: ", err)
+				return
 			}
+			defer out.Close()
 			_, err = io.Copy(out, file)
 			if err != nil {
 				log.Println("error saving file: ", err)
+				return
 			}
-			defer file.Close()
-			defer out.Close()
 			log.Println("grabbing lock...")
 			sem.Lock()
 			log.Println("got lock, updating package list...")
@@ -240,8 +246,10 @@ func uploadHandler(config Conf) http.Handler {
 			}
 			sem.Unlock()
 			log.Println("lock returned")
+			return
 		} else {
 			log.Println("not a POST")
+			return
 		}
 	})
 }
